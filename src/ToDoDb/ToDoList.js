d14ac4eedd775f8101982a4afaff2aeb464918c4
Halil İbrahim NERGİZ <halil123911@gmail.com>
2021-11-04 19:34:34 +0300

    interface updated

--- src/ToDoDb/ToDoList.js
+++ src/ToDoDb/ToDoList.js
@@ -2,10 +2,10 @@ import Task from "../Models/TaskModel";
 
 const TaskList = [
 
-    new Task('Alışveriş', 'Beslenme Çantası', new Date(2000, 3, 2 ), 'Ev', false,0),
-    new Task('Alışveriş', 'Bahçe Takımı', new Date(2016,5,18), 'Bahçe', false,1),
-    new Task('Alışveriş', 'Ofis Takımı', new Date(2014,11,3), 'Ofis', false,2),
-    new Task('Alışveriş', 'aasdfasdfasdf asdfasdfasdfakjshlfas şlkajsdlfkgjhasdlf şlkajsdlfkgjhasdlf şlkajsdlfkgjhasdlf şlkajsdlfkgjhasdlf şlkajsdlfkgjhasdlf lkjahsdfklh lkajushdfklj', new Date(2030,6,28), 'Ofis', false,3),
+    new Task('Alışveriş', 'Beslenme Çantası', new Date(2000, 3, 2), 'Ev', false, 0),
+    new Task('Alışveriş', 'Bahçe Takımı', new Date(2016, 5, 18), 'Bahçe', false, 1),
+    new Task('Alışveriş', 'aasdfasdfasdf asdfasdfasdfakjshlfas şlkajsajsdlfkgjhasdlf şlkajsdlfkgjhasdlf şlkajsdlajsdlfkgjhasdlf şlkajsdlfkgjhasdlf şlkajsdlajsdlfkgjhasdlf şlkajsdlfkgjhasdlf şlkajsdldlfkgjhasdlf şlkajsdlfkgjhasdlf şlkajsdlfkgjhasdlf şlkajsdlfkgjhasdlf şlkajsdlfkgjhasdlf lkjahsdfklh lkajushdfklj', new Date(2030, 6, 28), 'Ofis', false, 3),
+    new Task('Alışveriş', 'Ofis Takıs Takıms Takıms Takıms Takıms Takıms Takıms Takıms Takıms Takıms Takıms Takımmı', new Date(2014, 11, 3), 'Ofis', false, 2),
 ]
 
 export default TaskList;
--- src/view/PageView.js
+++ src/view/PageView.js
@@ -4,87 +4,162 @@ import Task from "../Models/TaskModel";
 
 
 export default class PageView extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { currentTaskList: TaskList };
+    }
 
-    Date() {
+    Date = () => {
         let today = new Date();
         return (`${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`)
     }
 
-    ResetValues() {
+    ResetValues = () => {
         document.querySelector('.task-title').value = ''
         document.querySelector('.task-content').value = ''
         document.querySelector('.task-date').value = ''
     }
 
 
+    openMap = (e) => {
+        const taskList = document.querySelector('.taskList');
+        const map = document.querySelector('.map');
+        // const control = document.querySelector('#task-click')
+        if (e.target.checked === true) {
+            taskList.style.position = 'relative'
+            taskList.style.left = '350px'
 
-
-    render() {
-
-        function AddTask(e) {
-            e.preventDefault();
-            const title = document.querySelector('.task-title').value;
-            const content = document.querySelector('.task-content').value;
-            const date = document.querySelector('.task-date').value;
-
-            TaskList.unshift(new Task(title, content, new Date(date)));
-
-            return (console.log(TaskList));
+            map.style.left = '-20rem'
+            map.style.opacity = '1'
+        }
+        else {
+            taskList.style.position = 'relative'
+            taskList.style.left = '0'
+            map.style.left = '-75rem'
+            map.style.opacity = '0'
 
         }
+    }
 
+    AddTask = (e) => {
+        e.preventDefault();
+        const title = document.querySelector('.task-title');
+        const content = document.querySelector('.task-content');
+        const date = document.querySelector('.task-date');
+        const newTask = new Task(title.value, content.value, new Date(date.value));
+
+        if (title.value.trim() === '' || content.value.trim() === '' || date.value.trim() === '') {
+            alert('You Must Enter All Values')
+        } else {
+            TaskList.unshift(newTask);
+            this.setState({ currentTaskList: TaskList });
+            alert('Task Created Successfully')
+        }
+
+
+        // if (title.textContent.length > 2) {
+        //     console.log(title.value);
+        // }
+        // console.log(title.textContent);
+    }
+
+
+    deleteAllTasks = (e) => {
+        e.preventDefault();
+        const control = window.confirm('Do You Want to Delete All Tasks');
+        if (control === true) {
+            TaskList.length = 0;
+            this.setState({ currentTaskList: TaskList });
+            setTimeout( () => { window.alert('Deleted All Tasks') }, 70)
+        }
+    }
+
+    showMore = (e) => {
+        const thisContent = e.target.previousElementSibling;
+        const thisBox = e.target.parentElement.parentElement.parentElement.parentElement
+        thisContent.classList.toggle('active')
+        thisBox.classList.toggle('active')
+        console.log(thisBox);
+
+        if (thisContent.classList == 'content active') {
+            e.target.textContent = 'Read Less'
+        } else {
+            e.target.textContent = 'Read More'
+        }
+
+    }
+
+    render() {
         return (
             <div>
                 <div className='to-do-app'>
-
-                    <input type='checkbox' id='click' />
-                    <label className='create-btn' htmlFor='click'> Create New Task</label>
-                    <div id='overlay'></div>
-
-
-                    <div className='pop-up'>
-                        <form className='input-field'>
-                            <label htmlFor='task-title' > </label>
-                            <input type='text' className='task-title' name='task-title' placeholder='Title' autoComplete='off' required />
-
-                            <label htmlFor='task-content'> </label>
-                            <input type='text' className='task-content' name='task-content' placeholder='Content' autoComplete='off' required />
-
-                            <label htmlFor='task-date'> </label>
-                            <input type='date' className='task-date' name='task-date' min={this.Date()} required />
-
-                            <button type='submit' className='btn-add' onClick={AddTask} > Create </button>
-                            <label htmlFor='click' className='btn-close' onClick={this.ResetValues}>Close</label>
-
-                        </form>
-                    </div>
+                    <div className='main'>
+                        <input type='checkbox' id='crt-click' />
+                        <label className='create-btn' htmlFor='crt-click'> Create New Task</label>
+                        <div id='overlay'></div>
 
 
+                        <div className='pop-up'>
+                            <form className='input-field'>
+                                <label htmlFor='task-title' > </label>
+                                <input type='text' className='task-title' name='task-title' placeholder='Title' autoComplete='off' maxLength='30' />
 
-                    <div className='task-list'>
+                                <label htmlFor='task-content'> </label>
+                                <input type='text' className='task-content' name='task-content' placeholder='Content' autoComplete='off' maxLength='360' />
 
-                        <ul>
-                            {TaskList.map(task =>
-                                <li className='a-task'>
-                                    <div className='container'>
-                                        <div className='card'>
+                                <label htmlFor='task-date'> </label>
+                                <input type='date' className='task-date' name='task-date' min={this.Date()} />
+
+                                <button type='submit' className='btn-add' onClick={this.AddTask}> Create </button>
+                                <label htmlFor='crt-click' className='btn-close' onClick={this.ResetValues}>Close</label>
+
+                            </form>
+                        </div>
+
+
+
+                        <div className='task-list'>
+
+                            <ul className='taskList'>
+                                {TaskList.map((task,index)=>
+                                    <li className='a-task'>
+                                        <div className='container'>
+
+                                            <p className='task-ex-date'>{task.executionDate.getDate() + '-' + task.executionDate.getMonth() + '-' + task.executionDate.getFullYear()}</p>
+
                                             <label>
-                                                <input type='checkbox' id='task-click' />
-                                                <div className='box'>
-                                                    <div className='content' >
+                                                <input type='checkbox' id='task-click' className='open-map' onClick={this.openMap} />
+                                                <div className='box' >
+
+                                                    <div className='content'>
                                                         <h2 className='task-h2'>{task.taskCaption}</h2>
                                                         <p className='task-p'>{task.taskContent}</p>
-                                                        <p className='task-ex-date'  >{task.executionDate.getDate() + '-' + task.executionDate.getMonth() + '-' + task.executionDate.getFullYear()}</p>
                                                     </div>
+                                                    <label className='more' id='read-more' htmlFor='down-icon' onClick={this.showMore}>Read More</label>
+
                                                 </div>
                                             </label>
+
                                         </div>
-                                    </div>
-                                </li>
-                            )}
-                        </ul>
+                                    </li>
+                                )}
+                            </ul>
+
+
+                            <div className='map'>
+                                <div className='map-content'>
+
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <input type='checkbox' id='delete-all' onClick={this.deleteAllTasks} />
+                        <label className='delete-all-btn' htmlFor='delete-all'> Delete All Tasks</label>
 
                     </div>
+
+
 
                 </div>
             </div>
